5f674c8a3a3367ddf6b56172d38b29aa9a7c0a34
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-04-16 13:54:44 +0100

    Declare and Intialise Variables from table

---
+++ ciscoProject.js
@@ -0,0 +1,19 @@
+// Declare and Intialise Variables 
+var contact1 = {
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}
+
+var contact2 = {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+}
+
+var contact3 = {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu"
+}
+
